25987e7176c35cb0067313ad676204bf1933148d
Sean Olejar <solejar236@gmail.com>
2017-03-14 16:13:50 -0400

    added some stuff

--- filezilla/php/controller.php
+++ filezilla/php/controller.php
@@ -1,4 +1,10 @@
 <?php
+//including all the module php scripts
+include 'traffic_collector.php';
+include 'weather_collector.php';
+include 'map_communicator.php';
+include 'loc_converter.php';
+
 //this is the controller. it's gonna handle the flow
 
 //this guy stores the params
@@ -28,10 +34,16 @@ $which_feature;
 
 //collect args from argv
 //might be nice to make this a list.
+$user_params = $argv[1];
 
 //if forecast===1, get weather
-if($forecast===1){
-	$weather_forecast = shell_exec('/usr/bin/php weather_collector.php params_go_here');
+if($forecast==1){
+	$date = $user_params[7];
+	
+	//$weather_command = sprintf('/usr/bin/php weather_collector.php %s',$date);
+	//this is a func from 'weather_collector.php'
+	$weather_command = forecast_weather($date);
+	//$weather_forecast = shell_exec($weather_command);
 	user_params[3] =  $weather_forecast;
 }
 
@@ -50,7 +62,12 @@ $location_params = $all_params[0];
 //this is date,time, weather
 $condition_params = $all_params[1];
 
-$latlng_location = shell_exec('/usr/bin/php loc_converter.php paramsgohere');
+//this is the feature I'm on!
+$feature = $all_params[2];
+
+//$loc_command = sprintf('/usr/bin/php loc_converter.php %s, %s'
+$latlng_location = get_location($location_params,$feature);
+//$latlng_location = shell_exec('/usr/bin/php loc_converter.php paramsgohere');
 
 $traffic_sev_array = shell_exec('/usr/bin/php traffic_collector.php params');
 
