585d70c4b2220858b5b6bdf8ea9eabc7e40913d1
Sean Olejar <solejar236@gmail.com>
2017-03-14 14:27:09 -0400

    working on ctroller

--- filezilla/php/controller.php
+++ filezilla/php/controller.php
@@ -1,1 +1,32 @@
-hello
+<?php
+//this is the controller. it's gonna handle the flow
+
+//this guy stores the params
+class paramStorage{
+	$conditionParams
+	$locParams
+	
+	//this is where vals are stored
+	function storeParams($inputs){
+
+	}	
+	
+	function getParams($whichParam){
+	}
+}
+
+//collect args from argv
+
+//if forecast===1,
+if($forecast===1){
+	$wf = shell_exec('/usr/bin/php weather_collector.php params_go_here');
+	array_push($userParams, $wf)
+}
+
+paramStore = new paramStorage();
+
+paramStore.storeParams($userParams);
+
+
+
+?>
