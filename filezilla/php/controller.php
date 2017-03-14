4566ac74fb42765dc7e376a9ac4ea327bc573f6d
ridwankhan <ridwankhan101@gmail.com>
2017-03-14 16:14:44 -0400

    Parameter storage

--- filezilla/php/controller.php
+++ filezilla/php/controller.php
@@ -3,15 +3,89 @@
 
 //this guy stores the params
 class paramStorage{
-	$conditionParams
-	$locParams
+	private $conditionParams; 
+	private $locParams;
+	private $feature;
+	/*
+	0 - 1 Forecast first, bool, 0 or 1
+	l - loc1 - for heatmap, zipcode. For route, start location
+	2 - loc2 - for heatmap, range. For route, end location
+	3 - weather - for forecast ==1, null. For forecast ==0, from forms
+	4 - severity - same for both
+	5 - time - same for both
+	6 - day - for forecast ==0, take from form. For forecast ==1, parse from date form
+	7 - date - for forecast==0, null. For forecast ==1, read from form.
+	8 - whichFeature - ‘heatmap’,’route’,’agenda’ based on the folder where index.html is i.e. heatmap/index.html
+	*/
 	
 	//this is where vals are stored
-	function storeParams($inputs){
+	public function storeParams($inputs){
+		$feature = $inputs[8];
+
+		if ($feature == "heatmap")
+		{
+			if ($inputs[0] =="0"){
+				//user inputted weather
+				$userLoc = $inputs[1];
+				$range = $inputs[2];
+				$weather = $inputs[3];
+				$sevArray = $inputs[4];
+				$time = $inputs[5];
+				$day = $inputs[6];
+
+			}
+
+			else if ($input[0] == "1") {
+				$userLoc = $inputs[1];
+				$range = $inputs[2];
+				// $weather = get from weather collector
+				$sevArray = $inputs[4];
+				$time = $inputs[5];
+				$day = $inputs[6];
+
+				
+			}
+
+			$locParams = array($userLoc, $$range);
+			$conditionParams = array($weather, $sevArray, $time, $day);
+		} //end of heat map
+
+		else if ($feature == "route"){
+			if ($inputs[0] == "0"){
+				//user inputted weather
+				$startLoc = $inputs[1];
+				$endLoc = $inputs[2];
+				$weather = $inputs[3];
+				$sevArray = $inputs[4];
+				$time = $inputs[5];
+				$day = $inputs[6];
+
+
+
+
+			}
+
+			else if ($input[0] == "1") {
+				//forecasted weather
+				$startLoc = $inputs[1];
+				$endLoc = $inputs[2];
+				// $weather = get from weather collector
+				$sevArray = $inputs[4];
+				$time = $inputs[5];
+				$day = $inputs[6];
+
+			}
+			$locParams = array($startLoc, $endLoc);
+			$conditionParams = array($weather, $sevArray, $time, $day);
+
+		} //end of route 
+
+
 
 	}	
 	
-	function getParams($whichParam){
+	public function getParams($whichParam){
+
 	}
 }
 
